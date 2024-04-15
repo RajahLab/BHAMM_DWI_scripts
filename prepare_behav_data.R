d169c02a39fb4c54e222cb1e39dc38753780aa28
Rikki Lissaman <rikki.lissaman@outlook.com>
2024-04-15 19:29:27 -0400

    Update prepare_behav_data.R
    Update to reflect manuscript changes.

--- prepare_behav_data.R
+++ prepare_behav_data.R
@@ -1,6 +1,6 @@
 # Name: prepare_behav_data.R
 # Author: Rikki Lissaman
-# Last updated (dd/mm/yyyy): 02/01/2024
+# Last updated (dd/mm/yyyy): 15/04/2024
 #
 # Description: This R script contains code that imports data from the Brain 
 # Health and Memory at Midlife (BHAMM) study, filters out participants that do
@@ -8,9 +8,9 @@
 # key demographic information, and outputs IDs/data for later analysis.
 #
 # Inputs: 
-# (1) SPSS file of the BHAMM Master database (Last updated: October 2023).
+# (1) SPSS file of the BHAMM Master database (Last updated: January 2024).
 # (2) SPSS file of the BHAMM Session 2 (i.e., neuroimaging session) database 
-# (Last updated: October 2023).
+# (Last updated: January 2024).
 # (3) Excel spreadsheet used for DWI QC (Last updated: November 2023).
 #
 # Outputs: 
@@ -41,18 +41,18 @@ library(dplyr) # version 1.0.8
 
 # Import the BHAMM study Master database and "clean" the variable names (i.e., 
 # make them all lower-case with spaces replaced by underscores, etc.)
-master <- read_sav("input-file-name.sav") %>% clean_names() # n = 468
+master <- read_sav("input-file-name.sav") %>% clean_names() # n = 480
 
 # Import the BHAMM Session 2 database, "clean" the variable names, and keep only
 # variables that are already in the master database (note: the master database is 
 # more comprehensive)
 session_two <- read_sav("input-file-name.sav") %>% 
   clean_names() %>%
-  select(c("id", "dw1_scan_num")) # n = 261
+  select(c("id", "dw1_scan_num")) # n = 273
 
 # Using participant ID, join together the two databases. Only those with Session 
 # 2 data will be retained
-bhamm <- left_join(session_two, master, by = "id") # n = 261
+bhamm <- left_join(session_two, master, by = "id") # n = 273
 
 
 # Identify Initially Eligible Participants --------------------------------
@@ -60,7 +60,7 @@ bhamm <- left_join(session_two, master, by = "id") # n = 261
 # Filter the joined database by age, keeping only middle-aged participants. We
 # used a threshold that was consistent with that used in our prior work - see
 # https://doi.org/10.1523/JNEUROSCI.0663-23.2023
-bhamm <- filter(bhamm, s2_age >= 39.5 & s2_age < 65.5) # n = 188
+bhamm <- filter(bhamm, s2_age >= 39.5 & s2_age < 65.5) # n = 200
 
 # Given our focus on sex and menopause, filter out participants whose current 
 # gender identity does not match their sex assigned at birth. Note: for sex, 
@@ -69,27 +69,33 @@ bhamm <- filter(bhamm, s2_age >= 39.5 & s2_age < 65.5) # n = 188
 bhamm <- bhamm %>% filter((sex == -1 & enrol_gender == 1) | 
                           (sex == -1 & is.na(enrol_gender)) | 
                           (sex == 1 & enrol_gender == 2) | 
-                          (sex == 1 & is.na(enrol_gender))) # n = 187
+                          (sex == 1 & is.na(enrol_gender))) # n = 199
 
 # Filter out participants whose menopause status is listed as peri-menopausal 
 # ("2 [Perimenopause]" under `s2_meno_group`) or indeterminate ("4 [Indeterminate]" 
 # under `s2_meno_group`)
-bhamm <- filter(bhamm, !s2_meno_group %in% c(2, 4)) # n = 140
+bhamm <- filter(bhamm, !s2_meno_group %in% c(2, 4)) # n = 148
 
 # Filter out participants currently using hormone replacement therapy ("2 [Yes, 
 # currently]" under `hrt_use`
-bhamm <- filter(bhamm, !hrt_use %in% c(2)) # n = 130
+bhamm <- filter(bhamm, !hrt_use %in% c(2)) # n = 138
 
 # Filter out participants with diabetes ("-1 [Yes]" under `med_con_diabetes`)
-bhamm <- filter(bhamm, !med_con_diabetes %in% c(-1)) # n = 129
+bhamm <- filter(bhamm, !med_con_diabetes %in% c(-1)) # n = 137
 
 # Filter out participants who, for various reasons (e.g., scan dropped from 
 # sequence due to time, session cancelled, etc.) are listed as "NA"/NA for the 
 # variable `dw1_scan_num`. These participants do not have diffusion data
-bhamm <- filter(bhamm, !dw1_scan_num %in% c("NA", NA)) # n = 128
+bhamm <- filter(bhamm, !dw1_scan_num %in% c("NA", NA)) # n = 126
 
 # Filter out participants scanned with old DWI parameters (checked separately)
-bhamm <- filter(bhamm, !id %in% c(0001)) # n = 117
+bhamm <- filter(bhamm, !id %in% c(0001)) # n = 125
+
+# Filter out participants who have been recruited since the manuscript was 
+# originally submitted. This enables us to re-run our data on the amended dataset 
+# without adding participants who were recruited post-submission
+bhamm <- filter(bhamm, !id %in% c(0001, 0002, 0003, 0004, 
+                                  0005, 0006, 0007, 0008)) # n = 117
 
 
 # Obtain Demographic Data Prior to Further Exclusions ---------------------
@@ -102,35 +108,6 @@ bhamm %>%
             sd = round(sd(s2_age), 2),
             min = round(min(s2_age), 2),
             max = round(max(s2_age), 2))
-
-## Sex (Males/Females) ----
-
-# Count the number of males and females in the sample
-bhamm %>% 
-  mutate(sex = as_factor(sex)) %>%
-  count(sex)
-
-# Calculate age statistics (min, max, M, SD)
-bhamm %>% 
-  mutate(sex = as_factor(sex)) %>%
-  group_by(sex) %>%
-  summarise(mean = round(mean(s2_age), 2),
-            sd = round(sd(s2_age), 2),
-            min = round(min(s2_age), 2),
-            max = round(max(s2_age), 2))
-
-# Calculate mean years of education
-bhamm %>%
-  mutate(sex = as_factor(sex)) %>%
-  group_by(sex) %>%
-  summarise(mean = round(mean(edu), 2))
-
-# For males and females, count the different ethnicities included
-bhamm %>% 
-  mutate(sex = as_factor(sex),
-         ethnicity = as_factor(ethnicity)) %>%
-  group_by(sex) %>%
-  count(ethnicity)
 
 ## Menopause (Pre/Post) ----
 
@@ -163,6 +140,35 @@ bhamm %>%
   mutate(s2_meno_group = as_factor(s2_meno_group),
          ethnicity = as_factor(ethnicity)) %>%
   group_by(s2_meno_group) %>%
+  count(ethnicity)
+
+## Sex (Males/Females) ----
+
+# Count the number of males and females in the sample
+bhamm %>% 
+  mutate(sex = as_factor(sex)) %>%
+  count(sex)
+
+# Calculate age statistics (min, max, M, SD)
+bhamm %>% 
+  mutate(sex = as_factor(sex)) %>%
+  group_by(sex) %>%
+  summarise(mean = round(mean(s2_age), 2),
+            sd = round(sd(s2_age), 2),
+            min = round(min(s2_age), 2),
+            max = round(max(s2_age), 2))
+
+# Calculate mean years of education
+bhamm %>%
+  mutate(sex = as_factor(sex)) %>%
+  group_by(sex) %>%
+  summarise(mean = round(mean(edu), 2))
+
+# For males and females, count the different ethnicities included
+bhamm %>% 
+  mutate(sex = as_factor(sex),
+         ethnicity = as_factor(ethnicity)) %>%
+  group_by(sex) %>%
   count(ethnicity)
 
 
@@ -233,7 +239,7 @@ cooks_d <- data.frame(id = bhamm$id,
                       recog_easy_d = cooks.distance(lm_recog_easy),
                       recog_hard_d = cooks.distance(lm_recog_hard),
                       miss_easy_d = cooks.distance(lm_miss_easy),
-                      miss_hard_d = cooks.distance(lm_miss_hard)) 
+                      miss_hard_d = cooks.distance(lm_miss_hard)) # n = 96
 
 # For each measure (correct source, recognition, misses), determine whether a 
 # given participant's Cook D value is more than 3 standard deviations higher than 
